f16cdc2ce9501476b7ce06eb69dd21dc9435197a
weng lin <wenglin@buffalo.edu>
2019-12-17 23:31:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  key[value]# return a hash that includes the key and value parameters passed into this method
+  my_hash_creator[key]# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
