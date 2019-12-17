ceab167abf07565a0c970e3a54e40275e1154ac6
weng lin <wenglin@buffalo.edu>
2019-12-17 23:47:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if key
-    key += 1
+  if hash[key]
+    hash[key] += 1
   else
-    key = 1
+    hash[key] = 1
   end
 end
